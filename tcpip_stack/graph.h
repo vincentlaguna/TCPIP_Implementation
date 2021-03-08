2e6311aeeb30bd38de561552f02ea3ebcef7be8b
vincentlaguna <vincentlaguna@live.com>
2021-03-08 22:18:06 +0000

    Graph Data Structures - commit #2 - C

--- tcpip_stack/graph.h
+++ tcpip_stack/graph.h
@@ -3,9 +3,17 @@
 
 typedef struct interface_
 {
-  char if_name[IF_NAME_SIZE];
-  struct node_ * att_node;
+  char		if_name[IF_NAME_SIZE];
+  struct node_ 	*att_node;
+  struct link_ 	*link;
 } interface_t;
+
+struct link_
+{
+  interface_t	intf1;
+  interface_t	intf2;
+  unsigned int	cost;
+};
 
 typedef struct graph_
 {
