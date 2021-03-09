de8dd461c696654759ce45b1bcc751069c237714
vincentlaguna <vincentlaguna@live.com>
2021-03-09 22:36:07 +0000

    Graph Data Structures - commit #8 - C

--- tcpip_stack/graph.h
+++ tcpip_stack/graph.h
@@ -2,8 +2,8 @@
 #define 	__GRAPH_H__
 #include 	"gluethread/glthread.h"
 // Define Constants
-#define 	NODE_NAME_SIZE		16
-#define 	IF_NAME_SIZE		16
+#define 	NODE_NAME_SIZE    16
+#define 	IF_NAME_SIZE		  16
 #define		MAX_INTF_PER_NODE	10
 // Forward Declarations (to avoid recursive dependencies between Data Structures)
 typedef struct 	node_ node_t;
@@ -11,31 +11,35 @@ typedef struct 	link_ link_t;
 
 typedef struct 	interface_
 {
-  char		if_name[IF_NAME_SIZE];
+  char		      if_name[IF_NAME_SIZE];
   struct node_ 	*att_node;
   struct link_ 	*link;
 } interface_t;
 
 struct link_
 {
-  interface_t	intf1;
-  interface_t	intf2;
+  interface_t	  intf1;
+  interface_t	  intf2;
   unsigned int	cost;
 };
 
-struct 		node_
+struct 		      node_
 {
-  char		node_name[NODE_NAME_SIZE];
-  interface_t	*intf[MAX_INTF_PER_NODE];
-  glthread_t 	graph_glue;
+  char		      node_name[NODE_NAME_SIZE];
+  interface_t	  *intf[MAX_INTF_PER_NODE];
+  glthread_t 	  graph_glue;
 };
 
 GLTHREAD_TO_STRUCT(graph_glue_to_node, node_t, graph_glue);
 
 typedef struct 	graph_
 {
-  char 		topology_name[32];
-  glthread_t 	node_list;
-};
+  char 		      topology_name[32];
+  glthread_t 	  node_list;
+} graph_t;
+
+node_t          *create_graph_node(graph_t *graph, char *node_name);
+graph_t         *create_new_graph(char *topology_name);
+
 
 #endif // __GRAPH_H__
