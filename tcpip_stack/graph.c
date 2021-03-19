3e95aa9e30a7fa44da4f60aef854ab8abf658a0f
vincentlaguna <vincentlaguna@live.com>
2021-03-19 17:23:16 +0000

    Graph Data Structures - Generic Graph Construction - creation of create_graph api in graph.c - commit #4 - C

--- tcpip_stack/graph.c
+++ tcpip_stack/graph.c
@@ -1,5 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "graph.h"
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <string.h>
+#include  "graph.h"
 
+graph_t   *create_graph(char *topology_name)
+{
+  graph_t *graph = calloc(1, sizeof(graph_t));
+  strncpy(graph->topology_name, topology_name, 32);
+  graph->topology_name[31] = '\0';
+  
+  init_glthread(&graph->node_list);
+  return graph;
+}
