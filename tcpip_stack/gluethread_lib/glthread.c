ff8a0dcf5327ed994301d43155590ea7fbd47212
vincentlaguna <vincentlaguna@live.com>
2021-04-02 21:16:53 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - creation of gluethread library - commit #33 - C

--- tcpip_stack/gluethread_lib/glthread.c
+++ tcpip_stack/gluethread_lib/glthread.c
@@ -1,42 +1,61 @@
-#include                    <stdlib.h>
-#include                    "glthread.h"
+#include                            <stdlib.h>
+#include                            "glthread.h"
 
 void    init_glthread(glthread_t *glthread)
 {
-  glthread->left            =  NULL;
-  glthread->right           =  NULL; 
+  glthread->left                 =  NULL;
+  glthread->right                =  NULL; 
 }
 
 void    glthread_add_next(glthread_t *curr_glthread, glthread_t *new_glthread)
 {
   if (!curr_glthread->right)
   {
-    curr_glthread->right    =  new_glthread;
-    new_glthread->left      =  curr_glthread;
+    curr_glthread->right         =  new_glthread;
+    new_glthread->left           =  curr_glthread;
     return;
   }
   
-  glthread_t *temp          =  curr_glthread->right;
-  curr_glthread->right      =  new_glthread;
-  new_glthread->left        =  curr_glthread;
-  new_glthread->right       =  temp;
-  temp->left                =  new_glthread
+  glthread_t *temp               =  curr_glthread->right;
+  curr_glthread->right           =  new_glthread;
+  new_glthread->left             =  curr_glthread;
+  new_glthread->right            =  temp;
+  temp->left                     =  new_glthread
 }
 
 void    glthread_add_before(glthread_t *curr_glthread, glthread_t *new_glthread)
 {
   if (!curr_glthread->left)
   {
-    new_glthread->left      =  NULL;
-    new_glthread->right     =  curr_glthread;
-    curr_glthread->laft     =  new_glthread;
+    new_glthread->left           =  NULL;
+    new_glthread->right          =  curr_glthread;
+    curr_glthread->laft          =  new_glthread;
     return;
   }
   
-  glthread_t *temp          =  curr_glthread->left;
-  temp->right               =  new_glthread;
-  new_glthread->left        =  temp;
-  new_glthread->right       =  curr_glthread;
-  curr_glthread->left       =  new_glthread;
+  glthread_t *temp               =  curr_glthread->left;
+  temp->right                    =  new_glthread;
+  new_glthread->left             =  temp;
+  new_glthread->right            =  curr_glthread;
+  curr_glthread->left            =  new_glthread;
 }
 
+void    remove_glthread(glthread_t *curr_glthread)
+{
+  if (!curr_glthread->left)
+  {
+    if (curr_glthread->right)
+    {
+      curr_glthread->right->left =  NULL;
+      curr_glthread->right       =  0;
+      return;
+    }
+    return;
+  }
+  
+  if (!curr_glthread->right)
+  {
+    curr_glthread->left->right   =  NULL;
+    curr_glthread->left          =  NULL;
+  }
+}
