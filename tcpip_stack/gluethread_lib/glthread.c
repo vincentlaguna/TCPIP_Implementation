ca401ca9eee037f7c5feeed99414636203112b98
vincentlaguna <vincentlaguna@live.com>
2021-04-02 20:55:40 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - creation of gluethread library - commit #31 - C

--- tcpip_stack/gluethread_lib/glthread.c
+++ tcpip_stack/gluethread_lib/glthread.c
@@ -1,7 +1,25 @@
-#include <stdlib.h>
-#include "glthread.h"
+#include                    <stdlib.h>
+#include                    "glthread.h"
 
-void init_glthread(glthread_t *glthread)
+void    init_glthread(glthread_t *glthread)
 {
-  
+  glthread->left            =  NULL;
+  glthread->right           =  NULL; 
 }
+
+void    glthread_add_next(glthread_t *curr_glthread, glthread_t *new_glthread)
+{
+  if (!curr_glthread->right)
+  {
+    curr_glthread->right    =  new_glthread;
+    new_glthread->left      =  curr_glthread;
+    return;
+  }
+  
+  glthread_t *temp          =  curr_glthread->right;
+  curr_glthread->right      =  new_glthread;
+  new_glthread->left        =  curr_glthread;
+  new_glthread->right       =  temp;
+  temp->left                =  new_glthread
+}
+
