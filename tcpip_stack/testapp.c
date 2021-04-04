4f47e8cc0627c11d2cc2567eca218e3603853c19
vincentlaguna <vincentlaguna@live.com>
2021-04-04 23:06:49 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - completion of Makefile - commit #40 - C

--- tcpip_stack/testapp.c
+++ tcpip_stack/testapp.c
@@ -1,1 +1,10 @@
-#include "graph.h"
+#include          "graph.h"
+
+extern graph_t *build_first_topo();
+
+int main(void)
+{
+  graph_t *topo   =  build_first_topo();
+  dump_graph(topo);
+  return(0);
+}
