477f9663124609739a019b873c1ea5e7b22c8a7f
vincentlaguna <vincentlaguna@live.com>
2021-04-05 20:46:59 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - function implementation: dump_graph, dump_node and dump_interface - commit #42 - C

--- tcpip_stack/graph.c
+++ tcpip_stack/graph.c
@@ -1,13 +1,13 @@
-#include                        <stdio.h>
-#include                        <stdlib.h>
-#include                        <string.h>
-#include                        "graph.h"
+#include                            <stdio.h>
+#include                            <stdlib.h>
+#include                            <string.h>
+#include                            "graph.h"
 
 graph_t   *create_new_graph(char *topology_name)
 {
   graph_t *graph = calloc(1, sizeof(graph_t));
   strncpy(graph->topology_name, topology_name, 32);
-  graph->topology_name[31] = '\0';
+  graph->topology_name[31]          = '\0';
   
   init_glthread(&graph->node_list);
   return graph;
@@ -17,7 +17,7 @@ node_t  *create_graph_node(graph_t *graph, char *node_name)
 {
   node_t *node = calloc(1, sizeof(node_t));
   strncpy(node->node_name, node_name, NODE_NAME_SIZE);
-  node->node_name[NODE_NAME_SIZE] = '\0';
+  node->node_name[NODE_NAME_SIZE]   = '\0';
   
   init_glthread(&graph->node_list, &node->graph_glue);
   return node;
@@ -36,21 +36,21 @@ void insert_link_between_two_nodes(node_t *node1,
   strncpy(link->intf2.if_name, to_if_name, IF_NAME_SIZE);
   link->intf2.if_name[IF_NAME_SIZE] = '\0';
   // Set back pointer to link to access the neighbour node on any given interface
-  link->intf1.link              =  link; 
-  link->intf2.link              =  link;
+  link->intf1.link                  =  link; 
+  link->intf2.link                  =  link;
   // Attach the interface to the node to wich each interface has been plugged in
-  link->intf1.att_node          =  node1; 
-  link->intf2.att_node          =  node2; 
+  link->intf1.att_node              =  node1; 
+  link->intf2.att_node              =  node2; 
   
-  link->cost                    =  cost;
+  link->cost                        =  cost;
   
   int empty_intf_slot;
   // Plugin interface ends into Node*
-  empty_intf_slot = get_node_intf_available_slot(node1);
-  node1->intf1[empty_intf_slot] = &link->intf1;
+  empty_intf_slot                   = get_node_intf_available_slot(node1);
+  node1->intf1[empty_intf_slot]     = &link->intf1;
   
-  empty_intf_slot = get_node_intf_available_slot(node2);
-  node2->intf2[empty_intf_slot] = &link->intf2;
+  empty_intf_slot                   = get_node_intf_available_slot(node2);
+  node2->intf2[empty_intf_slot]     = &link->intf2;
 }
 
 void dump_graph(graph_t *graph)
