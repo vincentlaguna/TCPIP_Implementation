a8ebc87b903ae4c8c5a85774b7664668a1bce04e
vincentlaguna <vincentlaguna@live.com>
2021-04-08 21:52:13 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - branching into some linked list review and have the option for LL implementation instead of gluethreads - commit #59 - C

--- tcpip_stack/LL_API/LL_API.h
+++ tcpip_stack/LL_API/LL_API.h
@@ -2,10 +2,10 @@
 #define __LL_API__
 
 #define GET_HEAD_SINGLY_LL(linked_list) (linked_list->head)
-#define INC_NODE_COUNT_SINGLY_LL(linked_list) (linked_list->node_count++)
-#define DEC_NODE_COUNT_SINGLY_LL(linked_list) (linked_list->node_count--)
-#define GET_NODE_COUNT_SINGLY_LL(linked_list) (linked_list->node_count)
-#define GET_NEXT_NODE_SINGLY_LL(node) (node->next)
+#define INC_NODE_COUNT_LL(linked_list) (linked_list->node_count++)
+#define DEC_NODE_COUNT_LL(linked_list) (linked_list->node_count--)
+#define GET_NODE_COUNT_LL(linked_list) (linked_list->node_count)
+#define GET_NEXT_NODE_LL(node) (node->next)
 
 typedef enum
 {
@@ -29,3 +29,4 @@ typedef struct linked_list
 linked_list_t *init_single_linked_list(void);
 linked_list_node_t *linked_list_init_node(void *data);
 int linked_list_add_node(linked_list_t *linked_list, linked_list_node_t *node);
+int linked_list_add_node_by_val(linked_list_t *linked_list, void *data);
