68b3d98a5bb59e509c8fded345f70b25d41afdeb
vincentlaguna <vincentlaguna@live.com>
2021-04-08 21:05:34 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - branching into some linked list review and have the option for LL implementation instead of gluethreads - commit #56 - C

--- tcpip_stack/LL_API/LL_API.h
+++ tcpip_stack/LL_API/LL_API.h
@@ -1,3 +1,15 @@
 #ifndef __LL_API__
 #define __LL_API__
 
+#define GET_HEAD_SINGLY_LL(linked_list) (linked_list->head)
+#define INC_NODE_COUNT_SINGLY_LL(linked_list) (linked_list->node_count++)
+#define DEC_NODE_COUNT_SINGLY_LL(linked_list) (linked_list->node_count--)
+#define GET_NODE_COUNT_SINGLY_LL(linked_list) (linked_list->node_count)
+#define GET_NEXT_NODE_SINGLY_LL(node) (node->next)
+
+typedef enum
+{
+  LL_FALSE,
+  LL_TRUE
+} bool_t;
+
