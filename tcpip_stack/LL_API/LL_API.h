a8fb1beee6ee18701dbb87be4324a619df76ae6f
vincentlaguna <vincentlaguna@live.com>
2021-04-08 21:35:13 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - branching into some linked list review and have the option for LL implementation instead of gluethreads - commit #58 - C

--- tcpip_stack/LL_API/LL_API.h
+++ tcpip_stack/LL_API/LL_API.h
@@ -15,7 +15,17 @@ typedef enum
 
 typedef struct linked_list_node
 {
-  void    *data;
-  struct  linked_list_node *next;
-} single_linked_list_node_t;
+  void                      *data;
+  struct  linked_list_node  *next;
+} linked_list_node_t;
 
+typedef struct linked_list
+{
+  unsigned int              node_count;
+  linked_list_node_t        *head;
+  int (*comparison_fn)(void *, void *);
+} linked_list_t;
+
+linked_list_t *init_single_linked_list(void);
+linked_list_node_t *linked_list_init_node(void *data);
+int linked_list_add_node(linked_list_t *linked_list, linked_list_node_t *node);
