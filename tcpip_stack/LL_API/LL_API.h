50e4c708d753593e7423de847dc658c19225bfe9
vincentlaguna <vincentlaguna@live.com>
2021-04-09 21:30:25 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - branching into some linked list review and have the option for LL implementation instead of gluethreads - commit #64 - C

--- tcpip_stack/LL_API/LL_API.h
+++ tcpip_stack/LL_API/LL_API.h
@@ -30,10 +30,16 @@ linked_list_t *init_single_linked_list(void);
 linked_list_node_t *linked_list_init_node(void *data);
 int  linked_list_add_node(linked_list_t *linked_list, linked_list_node_t *node);
 int  linked_list_add_node_by_val(linked_list_t *linked_list, void *data);
-int  linked_list_remove_node(linked_list_t *linked_list, linked_list_node_t *node);
+unsigned int  linked_list_remove_node(linked_list_t *linked_list, linked_list_node_t *node);
 int  linked_list_remove_node_by_val(linked_list_t *linked_list, void *data, int size);
 bool_t is_linked_list_empty(linked_list_t *linked_list);
 void print_linked_list(linked_list_t *linked_list);
 void reverse_linked_list(linked_list_t *linked_list);
 void delete_linked_list(linked_list_t *linked_list);
 int  linked_list_delete_node(linked_list_t *linked_list, linked_list_node_t *node);
+unsigned int  linked_list_delete_node_by_val(linked_list_t *linked_list, void *data, int size);
+linked_list_node_t *linked_list_get_node_by_data_ptr(linked_list_t *linked_list, void *data);
+unsigned int  linked_list_remove_node_by_data_ptr(linked_list_t *linked_list, void *data);
+void linked_list_set_comparison_fn(linked_list_t *linked_list, int (*comparison_fn)(void *, void*));
+void *linked_list_search_by_key(linked_list_t *linked_list, void *key);
+
