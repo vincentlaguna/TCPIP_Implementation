092919d954941e8ddd535074f639cf647f7cb61e
vincentlaguna <vincentlaguna@live.com>
2021-04-11 16:31:42 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - branching into some linked list review and have the option for LL implementation instead of gluethreads - LL_API.c implementation - commit #69 - C

--- tcpip_stack/LL_API/LL_API.c
+++ tcpip_stack/LL_API/LL_API.c
@@ -35,7 +35,12 @@ int  linked_list_add_node(linked_list_t *linked_list, linked_list_node_t *node)
   return 0;
 }
 
-int  linked_list_add_node_by_val(linked_list_t *linked_list, void *data);
+int  linked_list_add_node_by_val(linked_list_t *linked_list, void *data)
+{
+  linked_list_node_t *node = linked_list_init_node(data);
+  return linked_list_add_node(linked_list, node);
+}
+
 unsigned int  linked_list_remove_node(linked_list_t *linked_list, linked_list_node_t *node);
 int  linked_list_remove_node_by_val(linked_list_t *linked_list, void *data, int size);
 bool_t is_linked_list_empty(linked_list_t *linked_list);
