a8ef38dcb98310d51bee501bbbe47fa711ad781f
vincentlaguna <vincentlaguna@live.com>
2021-04-18 18:08:16 +0000

    Graph Data Structures - Generic Graph Construction - creation of topologies.c - build first topology - branching into some linked list review and have the option for LL implementation instead of gluethreads - LL_main.c implementation - commit #92 - C

--- tcpip_stack/LL_API/LL_main.c
+++ tcpip_stack/LL_API/LL_main.c
@@ -9,3 +9,19 @@ typedef struct person_
   int  age;
   int  weight;
 } person_t;
+
+int person_comparison_fn(void *pStruct, void *key)
+{
+  person_t *person = (person_t *)pStruct;
+  if (strncmp(person->name, (char *)key, strlen((char *)key)) == 0 &&
+      strlen((char *)key) == strlen(person->name))
+    return 1; // We found a match
+  return 0; // We did not find a match
+}
+
+void print_person_details(person_t *outperson)
+{
+  printf("outperson->name = %s\n", outperson->name);
+  printf("outperson->age = %d\n", outperson->age);
+  printf("outperson->name = %d\n", outperson->weight);
+}
