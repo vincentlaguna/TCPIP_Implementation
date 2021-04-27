abe3fdf2bda0d09cbe8c2dc76de4574eda842616
vincentlaguna <vincentlaguna@live.com>
2021-04-27 22:16:03 +0000

    Graph Data Structures - Implementation of CLI library - net.h implementation - commit #6 - C

--- tcpip_stack/net.h
+++ tcpip_stack/net.h
@@ -1,6 +1,21 @@
 #ifndef  __NET_H__
 #define  __NET_H__
 
-//Include new net.h structs
+typedef struct  ip_add_
+{
+  char          ip_addr[16];
+} ip_add_t;
+
+typedef struct  mac_add_
+{
+  char          mac[48];
+} mac_add_t;
+
+typedef struct  node_nw_prop_
+{
+  //Loop-Back Properties
+  bool_t        is_lb_addr_config;
+  ip_add_t      lb_addr; // Loop-back address of node
+}
 
 #endif //__NET_H__
