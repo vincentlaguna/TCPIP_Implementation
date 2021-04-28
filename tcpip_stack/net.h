c5d3f6380b03e45d3eb593ae3595f98b290bbe0c
vincentlaguna <vincentlaguna@live.com>
2021-04-28 22:32:39 +0000

    Graph Data Structures - Implementation of CLI library - net.h implementation - commit #9 - C

--- tcpip_stack/net.h
+++ tcpip_stack/net.h
@@ -25,6 +25,16 @@ static inline void  init_node_nw_prop(node_nw_prop_t *node_nw_prop)
   memset(node_nw_prop->lb_addr.ip_addr, 0, 16);
 }
 
+typedef struct  intf_nw_props_
+{
+  // L2 Properties
+  mac_add_t     mac_add; // MAC's are hard-coded into the interface NIC
+  // L3 Properties
+  // Set to TRUE if ip address is configures, intf operates in L3 mode if ip address is configured on it
+  bool_t        is_ipadd_config;
+  ip_add_t      ip_add;
+  char          mask;
+} intf_nw_props_t;
 
 
 #endif //__NET_H__
