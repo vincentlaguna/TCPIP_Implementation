fd923c2585174763fafe9ee51398dede439e72e7
vincentlaguna <vincentlaguna@live.com>
2021-05-02 03:17:12 +0000

    Graph Data Structures - Implementation of CLI library - net.c implementation - commit #19 - C

--- tcpip_stack/net.c
+++ tcpip_stack/net.c
@@ -8,12 +8,13 @@ void  interface_assign_mac_address(interface_t *interface)
   
 }
 
+bool_t  node_set_device_type(node_t *node, unsigned int F)
+{
+  
+}
+
 bool_t  node_set_loopback_address(node_t *node, char ip_addr)
 {
   
 }
 
-bool_t  node_set_device_type(node_t *node, unsigned int F)
-{
-  
-}
