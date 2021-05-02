3de61d1d4df6b26f1e4ce4b246b2b11153a5a754
vincentlaguna <vincentlaguna@live.com>
2021-05-02 03:22:00 +0000

    Graph Data Structures - Implementation of CLI library - net.c implementation - commit #20 - C

--- tcpip_stack/net.c
+++ tcpip_stack/net.c
@@ -5,7 +5,9 @@
 // Heuristics; Assign a unique MAC address to interface
 void  interface_assign_mac_address(interface_t *interface)
 {
-  
+  memset(IF_MAC(interface), 0, 48);
+  strcpy(IF_MAC(interface), interface->att_node->node_name);
+  strcat(IF_MAC(interface), interface->if_name);
 }
 
 bool_t  node_set_device_type(node_t *node, unsigned int F)
