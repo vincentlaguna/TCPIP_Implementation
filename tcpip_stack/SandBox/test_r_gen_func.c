4b80e9461c629dbaec1435ea0eb6bd8fad910379
vincentlaguna <vincentlaguna@live.com>
2021-05-06 22:15:19 +0000

    Graph Data Structures - Implementation of CLI library - net.c implementation - Random number generator function - commit #37 - C

---
+++ tcpip_stack/SandBox/test_r_gen_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+static unsigned int hash_code(void *ptr, unsigned int size)
+{
+  unsigned int value = 0, i = 0;
+  char *str = (char *)ptr;
+  
+  while(i < size)
+  {
+    value += *str;
+    value *= 97;
+    str++;
+    i++;
+  }
+  return value;
+}
+
+int main(void)
+{
+  char *str = "Random";
+  unsigned int size = 28;
+  
+  printf("Testing Random Number Generator Function\n\n");
+  for(int i = 0; i < 10; i++)
+  {
+    printf("%d\n", hash_code(str, size));
+  }
+  
+  return(0);
+}
