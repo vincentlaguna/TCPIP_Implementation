2a92c2ea20b4a4bcbc1556c54620f04f01369f87
vincentlaguna <vincentlaguna@live.com>
2021-05-09 00:38:50 +0000

    Graph Data Structures - Implementation of CLI library - net.c implementation - Random number generator function - commit #43 - C

--- tcpip_stack/topologies.c
+++ tcpip_stack/topologies.c
@@ -31,7 +31,20 @@ graph_t   *build_first_topo(void)
   insert_link_between_two_nodes(R1_re, R2_re, "eth0/2", "eth0/3", 1);
   insert_link_between_two_nodes(R0_re, R2_re, "eth0/4", "eth0/5", 1);
   
+  node_set_device_type(R0_re, L3_ROUTER);
   node_set_loopback_address(R0_re, "122.1.1.0");
+  node_set_intf_ip_address(R0_re, "eth0/4", "40.1.1.1", 24);
+  node_set_intf_ip_address(R0_re, "eth0/0", "20.1.1.1", 24);
+  
+  node_set_device_type(R0_re, L3_ROUTER);
+  node_set_loopback_address(R0_re, "122.1.1.1");
+  node_set_intf_ip_address(R0_re, "eth0/1", "20.1.1.2", 24);
+  node_set_intf_ip_address(R0_re, "eth0/2", "30.1.1.1", 24);
+  
+  node_set_device_type(R0_re, L3_ROUTER);
+  node_set_loopback_address(R0_re, "122.1.1.2");
+  node_set_intf_ip_address(R0_re, "eth0/3", "30.1.1.2", 24);
+  node_set_intf_ip_address(R0_re, "eth0/5", "40.1.1.2", 24);
   
   return topo;
 }
