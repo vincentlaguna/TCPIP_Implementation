da50a4f0a68504b9f3792f931f8fb768de1b3296
vincentlaguna <vincentlaguna@live.com>
2021-05-09 16:31:30 +0000

    Graph Data Structures - Assignment 4 - commit #1 - C

--- tcpip_stack/net.c
+++ tcpip_stack/net.c
@@ -32,6 +32,7 @@ void  interface_assign_mac_address(interface_t *interface)
   hash_code_value  *=   hash_code(interface->if_name, IF_NAME_SIZE);
   memset(IF_MAC(interface), 0, sizeof(IF_MAC(interface)));
   memcpy(IF_MAC(interface), (char *)&hash_code_value, sizeof(unsigned int));
+  // Need to review the following
   //strcpy(IF_MAC(interface), interface->att_node->node_name);
   //strcat(IF_MAC(interface), interface->if_name);
 }
