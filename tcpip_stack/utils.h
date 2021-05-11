f59028619e8b04582485318e87173f45e8b39dd6
vincentlaguna <vincentlaguna@live.com>
2021-05-11 23:03:51 +0000

    Graph Data Structures - Assignment 5 - utils.h implementation - commit #8 - C

--- tcpip_stack/utils.h
+++ tcpip_stack/utils.h
@@ -7,5 +7,10 @@ typedef   enum
   TRUE,
 } bool_t;
 
+#define   IS_BIT_SET(n, pos)    ((n & (1 << (pos))) != 0)
+#define   TOGGLE_BIT(n, pos)    (n = n ^ (1 << (pos)))
+#define   COMPLEMENT(num)       (num = num ^ 0xFFFFFFFF)
+#define   UNSET_BIT(n, pos)     (n = n & ((1 << pos) ^ 0xFFFFFFFF))
+#define   SET_BIT(n, pos)       (n = n | 1 << pos)
 
 #endif // __UTILS_H__
