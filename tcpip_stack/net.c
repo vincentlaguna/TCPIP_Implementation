40e7fcd1227fbac44b98fe4aaf081dc4c956ea92
vincentlaguna <vincentlaguna@live.com>
2021-05-12 21:54:31 +0000

    Graph Data Structures - Assignment 5 - Fixing Compile Errors - commit #3 - C

--- tcpip_stack/net.c
+++ tcpip_stack/net.c
@@ -50,7 +50,7 @@ bool_t  node_set_loopback_address(node_t *node, char *ip_addr)
   if(IS_BIT_SET(node->node_nw_prop.flags, HUB))
     assert(0); // Wrong cfg: No IP addreses on HUB
   
-  if(IS_BIT_SET(node->node_nw_prop.flags, L3_ROUTER))
+  if(!IS_BIT_SET(node->node_nw_prop.flags, L3_ROUTER))
     assert(0); // Must enable L3 routing first
   
   node->node_nw_prop.is_lb_addr_config = TRUE;
@@ -65,8 +65,7 @@ bool_t  node_set_intf_ip_address(node_t *node, char *local_if,
 {
   interface_t *interface = get_node_intf_by_name(node, local_if);
   
-  if(!interface)
-    assert(0);
+  if(!interface) assert(0);
     
   strncpy(IF_IP(interface), ip_addr, 16);
   IF_IP(interface)[16] = '\0';
--- tcpip_stack/testapp.c
+++ tcpip_stack/testapp.c
@@ -2,9 +2,9 @@
 
 extern graph_t *build_first_topo();
 
-int main(void)
+int main(int argc, char **argv)
 {
   graph_t *topo   =  build_first_topo();
-  dump_graph(topo);
+  dump_nw_graph(topo);
   return(0);
 }
