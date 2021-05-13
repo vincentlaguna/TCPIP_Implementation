75df532798afc5651dc984c52ddde225424412e8
vincentlaguna <vincentlaguna@live.com>
2021-05-13 00:56:15 +0000

    Graph Data Structures - Assignment 5 - Fixing Compile Errors: Faling at test.exe: net.c:68: node_set_intf_ip_address: Assertion 0 failed - commit #4 - C

--- tcpip_stack/net.c
+++ tcpip_stack/net.c
@@ -33,8 +33,8 @@ void  interface_assign_mac_address(interface_t *interface)
   memset(IF_MAC(interface), 0, sizeof(IF_MAC(interface)));
   memcpy(IF_MAC(interface), (char *)&hash_code_value, sizeof(unsigned int));
   // Need to review the following
-  //strcpy(IF_MAC(interface), interface->att_node->node_name);
-  //strcat(IF_MAC(interface), interface->if_name);
+  strcpy(IF_MAC(interface), interface->att_node->node_name);
+  strcat(IF_MAC(interface), interface->if_name);
 }
 
 bool_t  node_set_device_type(node_t *node, unsigned int F)
