4e03ed2298bcc6cdd984f51f455de3cf1048bbe7
vincentlaguna <vincentlaguna@live.com>
2021-05-15 02:11:53 +0000

    Graph Data Structures - Assignment 5 - Need to fix why Random MAC Addresses (net.c) are not being assigned... - commit #2 - C

--- tcpip_stack/SandBox/test_r_gen_func.c
+++ tcpip_stack/SandBox/test_r_gen_func.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-static unsigned int hash_code(void *ptr, unsigned int size)
-{
-  unsigned int value = 0, i = 0;
-  char *str = (char *)ptr;
+// static unsigned int hash_code(void *ptr, unsigned int size)
+// {
+//   unsigned int value = 0, i = 0;
+//   char *str = (char *)ptr;
   
-  while(i < size)
-  {
-    value += *str;
-    value *= 97;
-    str++;
-    i++;
-  }
-  return value;
-}
+//   while(i < size)
+//   {
+//     value += *str;
+//     value *= 97;
+//     str++;
+//     i++;
+//   }
+//   return value;
+// }
 
 int main(void)
 {
-  char *str = "Random";
-  unsigned int size = 28;
+  // char *str = "Random";
+  // unsigned int size = 28;
   
   printf("Testing Random Number Generator Function\n\n");
   for(int i = 0; i < 10; i++)
