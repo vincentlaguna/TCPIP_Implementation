052f7733109bf3456ae34539b387aa0f95260af4
vincentlaguna <vincentlaguna@live.com>
2021-05-15 19:44:02 +0000

    Graph Data Structures - Assignment 5.1 - utils.c implementation - commit #1 - C

---
+++ tcpip_stack/utils.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "utils.h"
+
+// Assignment 5.1 - Function that applies a mask on prefix and stores
+// the result in str_prefix - Don't forget to modify the Makefile to compile it
+// My Answer:
+void  apply_mask(char *prefix, char mask, char *str_prefix)
+{
+  //prefix - is a pointer to a string that represents an IP address
+  //str_prefix - is a pointer to a char array of size 16 bytes
+  //mask - is any value [0-32]
+  //The function must fill the str_prefix array with output
+  //The function must apply the mask on prefix and store the result in str_prefix
+  //i.e.: prefix = 122.1.1.1 -> mask(24) -> str_prefix stores 122.1.1.0
+}
+// Instructor Example:
